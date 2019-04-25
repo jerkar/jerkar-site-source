8b875698924c643f7b33f3455f1668f2a600ba74
angibaudj <angibaudj>
2019-04-25 17:09:25 +0200

    first import

--- jerkar/def/jerkar/github/io/Build.java
+++ jerkar/def/jerkar/github/io/Build.java
@@ -2,61 +2,51 @@ package jerkar.github.io;
 
 import org.jerkar.api.file.JkPathTree;
 import org.jerkar.api.java.JkJavaProcess;
-import org.jerkar.api.java.JkManifest;
 import org.jerkar.api.system.JkLog;
 import org.jerkar.api.utils.JkUtilsString;
 import org.jerkar.api.utils.JkUtilsTime;
-import org.jerkar.tool.JkBuild;
+
 import org.jerkar.tool.JkDoc;
 import org.jerkar.tool.JkInit;
+import org.jerkar.tool.JkRun;
 
 import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
-import java.util.jar.Attributes.Name;
 
-class Build extends JkBuild {
+class Build extends JkRun {
 
-    JkPathTree src = baseTree().goTo("src");
+    JkPathTree src = getBaseTree().goTo("src");
     JkPathTree markdownSrc = src.goTo("markdown");
     JkPathTree jbakeSrc = src.goTo("jbake");
 
-    JkPathTree jbakeToolDir = baseTree().goTo("build/tools/jbake-2.3.2");
-    JkPathTree siteBase = JkPathTree.of(outputDir().resolve("site"));
+    JkPathTree jbakeToolDir = getBaseTree().goTo("jerkar/tools/jbake-2.3.2");
+    JkPathTree siteBase = JkPathTree.of(getOutputDir().resolve("site"));
 
     // Source elements got from Jerkar project
-    Path jerkarProjectPath = baseDir().getParent().resolve("jerkar");
+    Path jerkarProjectPath = getBaseTree().getRoot().getParent().resolve("jerkar");
     JkPathTree jerkarCoreDocDir = JkPathTree.of(jerkarProjectPath.resolve("org.jerkar.core/src/main/doc"));
-    JkPathTree jerkarDistJavadoc = JkPathTree.of(jerkarProjectPath.resolve("org.jerkar.distrib-all/build/output/javadoc-all"));
-    Path jerkarDistZip =jerkarProjectPath.resolve("org.jerkar.distrib-all/build/output/jerkar-distrib.zip");
+    JkPathTree jerkarDistJavadoc = JkPathTree.of(jerkarProjectPath.resolve("org.jerkar.core/jerkar/output/javadoc-all"));
+    Path jerkarDistZip =jerkarProjectPath.resolve("org.jerkar.core/jerkar/output/org.jerkar.core-distrib.zip");
 
     JkPathTree jbakeSrcContent = jbakeSrc.goTo("content");
     JkPathTree siteSourceDocDir = jbakeSrcContent.goTo("documentation");
-    Path siteDistDir = siteBase.root().resolve("binaries");
+    Path siteDistDir = siteBase.getRoot().resolve("binaries");
     JkPathTree siteTargetDocDir = siteBase.goTo("content/documentation");
 
-    JkPathTree filesWithoutJbakeHeader = jbakeSrcContent.accept("**/*.md").refuse("about.md", "download.md",
+    JkPathTree filesWithoutJbakeHeader = jbakeSrcContent.andAccept(("**/*.md")).andReject("about.md", "download.md",
             "tell-me-more.md");
 
-    JkPathTree filesToAddSideMenu = filesWithoutJbakeHeader.refuse("documentation/latest/faq.md");
+    JkPathTree filesToAddSideMenu = filesWithoutJbakeHeader.andReject("documentation/latest/faq.md");
 
     @Override
     public void clean() {
-        siteBase.refuse(".*/**", "_*/**", "binaries/**").deleteContent();
+        siteBase.andReject(".*/**", "_*/**", "binaries/**").deleteContent();
         jbakeSrcContent.createIfNotExist().deleteContent();
-    }
-
-    @Override
-    public void doDefault() {
-        try {
-            full();
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
     }
 
     @JkDoc({ "Generates the site and imports documentation inside.",
@@ -78,14 +68,14 @@ class Build extends JkBuild {
 
     public void importDocFromJerkarProject() throws IOException {
         JkPathTree targetDocDir = siteSourceDocDir.goTo("latest");
-        List<Path> files = jerkarCoreDocDir.accept("**/*.md").refuse("reference/**/*").files();
+        List<Path> files = jerkarCoreDocDir.andAccept("**/*.md").andReject("reference/**/*").getFiles();
         Path temp = Files.createTempFile("reference", ".md");
-        jerkarCoreDocDir.goTo("reference").copyIn(temp);
+        jerkarCoreDocDir.goTo("reference").bring(temp);
         files.add(temp);
         for (Path file : files) {
-            String relativePath = file.startsWith(jerkarCoreDocDir.root()) ?
-                    jerkarCoreDocDir.root().relativize(file).toString() : file.getFileName().toString();
-            Path copied = targetDocDir.root().resolve(relativePath);
+            String relativePath = file.startsWith(jerkarCoreDocDir.getRoot()) ?
+                    jerkarCoreDocDir.getRoot().relativize(file).toString() : file.getFileName().toString();
+            Path copied = targetDocDir.getRoot().resolve(relativePath);
             Files.createDirectories(copied.getParent());
             JkLog.info("Importing doc file " + file + " to " + copied);
             byte[] content = Files.readAllBytes(file);
@@ -95,7 +85,7 @@ class Build extends JkBuild {
     }
 
     public void addJbakeHeaders() throws IOException {
-        for (Path file : filesWithoutJbakeHeader.files()) {
+        for (Path file : filesWithoutJbakeHeader.getFiles()) {
             String content = jbakeHeader(file);
             byte[] previousContent = Files.readAllBytes(file);
             content = content + new String(previousContent, Charset.forName("UTF8"));
@@ -105,17 +95,19 @@ class Build extends JkBuild {
 
     public void importSiteDoc() throws IOException {
         Path dest = jbakeSrc.get("content");
-        for (Path path : markdownSrc.files()) {
+        for (Path path : markdownSrc.getFiles()) {
             String originalContent = new String(Files.readAllBytes(path), Charset.forName("UTF8"));
             String newContent = originalContent.replace("${jerkarVersion}", currentJerkarVersion());
-            Path targetFile = dest.resolve(markdownSrc.root().relativize(path));
+            Path targetFile = dest.resolve(markdownSrc.getRoot().relativize(path));
             Files.createDirectories(targetFile.getParent());
             Files.write(targetFile, newContent.getBytes(Charset.forName("UTF8")), StandardOpenOption.CREATE_NEW);
         }
     }
 
     public void copyCurrentDist() {
-        JkPathTree.of(siteDistDir).copyIn(jerkarDistZip);
+        JkLog.execute("copying current dir",() -> JkPathTree.of(siteDistDir)
+                .bring(jerkarDistZip, StandardCopyOption.REPLACE_EXISTING));
+
 
         // Retrieve the current version
 
@@ -134,12 +126,17 @@ class Build extends JkBuild {
     }
 
     public void copyCurrentJavadoc() {
-        jerkarDistJavadoc.copyTo(siteBase.get("javadoc/latest"));
+        if (jerkarDistJavadoc.exists()) {
+            JkLog.execute("Copping javadoc", () -> jerkarDistJavadoc.copyTo(siteBase.get("javadoc/latest")));
+        } else {
+            JkLog.warn("Javadoc not found.");
+        }
     }
 
     public void jbake() {
-        JkJavaProcess.of().withClasspaths(jbakeToolDir.accept("lib.*.jar").files()).runJarSync(jbakeToolDir.get("jbake-core.jar"),
-                "src/jbake", "build/output/site");
+        JkJavaProcess.of().withClasspath(jbakeToolDir.andAccept("lib/*.jar").getFiles())
+                .runJarSync(jbakeToolDir.get("jbake-core.jar"),
+                "src/jbake", "jerkar/output/site");
     }
 
     private static String jbakeHeader(Path file) {
@@ -152,17 +149,18 @@ class Build extends JkBuild {
         return result.toString();
     }
 
-    public static void main(String[] args) {
-        JkInit.instanceOf(Build.class, args).doDefault();
+    public static void main(String[] args) throws IOException {
+        JkInit.instanceOf(Build.class, args).full();
     }
 
     private void addMenu() throws IOException {
-        for (Path file : filesToAddSideMenu.files()) {
+        for (Path file : filesToAddSideMenu.getFiles()) {
             String menuHtml = ImplicitMenu.ofMarkdowndFile(file, 2).divSideBarAndScriptHtml();
             byte[] currentContent = Files.readAllBytes(file);
             Files.write(file, menuHtml.getBytes("UTF8"), StandardOpenOption.CREATE);
             Files.write(file, currentContent, StandardOpenOption.APPEND);
-            Files.write(file, ImplicitMenu.endDivHtml("end of wrapper div").getBytes(Charset.forName("UTF8")), StandardOpenOption.APPEND);
+            Files.write(file, ImplicitMenu.endDivHtml("end of wrapper div").getBytes(Charset.forName("UTF8")),
+                    StandardOpenOption.APPEND);
         }
     }
 
